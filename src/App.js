909af9e7a1ec56935fb0df0ec440c6824c5957fc
sajanthomas01 <sajanthomas93@gmail.com>
2020-09-18 20:05:25 +0530

    added onPosChange to get the current and updated position

--- src/App.js
+++ src/App.js
@@ -2,53 +2,59 @@ import React from "react";
 import { Draggable } from "./lib";
 import "./App.css";
 class App extends React.Component {
-	state = {
-		words: ["Hello", "Hi", "How are you", "Cool"],
-		languages: ["C", "C++", "Java", "JS"],
-		shows: ["GOT", "Friends", "Big Bang"]
-	};
-	render() {
-		return (
-			<Draggable>
-				<div className="row">
-					<p className="text">Words</p>
-					<Draggable>
-						{this.state.words.map((word, idx) => {
-							return (
-								<div key={idx} className="flex-item">
-									{word}
-								</div>
-							);
-						})}
-					</Draggable>
-				</div>
-				<div className="row">
-					<p className="text">Languages</p>
-					<Draggable>
-						{this.state.languages.map((lng, idx) => {
-							return (
-								<div key={idx} className="flex-item">
-									{lng}
-								</div>
-							);
-						})}
-					</Draggable>
-				</div>
-				<div className="row">
-					<p className="text">Shows</p>
-					<Draggable>
-						{this.state.shows.map((lng, idx) => {
-							return (
-								<div key={idx} className="flex-item">
-									{lng}
-								</div>
-							);
-						})}
-					</Draggable>
-				</div>
-			</Draggable>
-		);
-	}
+  state = {
+    words: ["Hello", "Hi", "How are you", "Cool"],
+    languages: ["C", "C++", "Java", "JS"],
+    shows: ["GOT", "Friends", "Big Bang"],
+  };
+  
+  getChangedPos = (currentPos, newPos) => {
+    console.log(currentPos, newPos);
+  };
+
+  render() {
+   
+    return (
+      <Draggable>
+        <div className="row">
+          <p className="text">Words</p>
+          <Draggable onPosChange={this.getChangedPos}>
+            {this.state.words.map((word, idx) => {
+              return (
+                <div key={idx} className="flex-item">
+                  {word}
+                </div>
+              );
+            })}
+          </Draggable>
+        </div>
+        <div className="row">
+          <p className="text">Languages</p>
+          <Draggable>
+            {this.state.languages.map((lng, idx) => {
+              return (
+                <div key={idx} className="flex-item">
+                  {lng}
+                </div>
+              );
+            })}
+          </Draggable>
+        </div>
+        <div className="row">
+          <p className="text">Shows</p>
+          <Draggable>
+            {this.state.shows.map((lng, idx) => {
+              return (
+                <div key={idx} className="flex-item">
+                  {lng}
+                </div>
+              );
+            })}
+          </Draggable>
+        </div>
+      </Draggable>
+    );
+  }
 }
 
 export default App;
